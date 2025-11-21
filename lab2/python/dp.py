b7fb28a99150e537ce6e47e46d3664641bcc3beb
Alex Ionescu <github@ionescu.sh>
2025-11-21 14:42:00 +0100

    lab2: Update project templates

--- lab2/python/dp.py
+++ lab2/python/dp.py
@@ -1,33 +1,30 @@
 """
-Main module for the Differential Privacy DSL
+Differential Privacy DSL - Main module
 
-This module re-exports all the main classes and functions students will need.
-Import from this module in your code:
-
-    from dp import DP_DSL, create_dataset, Adult, Income, Occupation
-
-STUDENT TODO: Once you implement the core classes, this will work automatically.
+A unified implementation of a Domain Specific Language for Differential Privacy
+using the DP_DSL class that encapsulates budget management, transformations,
+and measurements in a single, easy-to-use interface.
 """
 
-# Core classes and functions
-from core import Dataset, DPResult, InsufficientBudget, InvalidParameter, create_dataset
+# Main DP_DSL class
+from dp_dsl import DP_DSL
 
-# Main DSL class
-from dp_dsl import DP_DSL, create_dsl
+# Core data structures
+from core import Dataset, create_dataset, DPResult, PrivacyError, InsufficientBudget, InvalidParameter, Epsilon, Budget
 
-# Adult dataset types and functions
+# Adult dataset utilities
 from adult_dataset import (
     Adult, WorkClass, Education, MaritalStatus, Occupation,
     Relationship, Race, Sex, Income, load_adult_dataset
 )
 
-# Export all public classes and functions
 __all__ = [
-    # Core types
-    'Dataset', 'DPResult', 'InsufficientBudget', 'InvalidParameter', 'create_dataset',
+    # Main DSL class
+    'DP_DSL',
 
-    # Main DSL
-    'DP_DSL', 'create_dsl',
+    # Core types
+    'Dataset', 'create_dataset', 'DPResult', 'PrivacyError', 'InsufficientBudget', 'InvalidParameter',
+    'Epsilon', 'Budget',
 
     # Adult dataset
     'Adult', 'WorkClass', 'Education', 'MaritalStatus', 'Occupation',
